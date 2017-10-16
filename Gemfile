e9d7077b347b7125cffba5af606d8d07ba60f417
Jean Pierre Perez Varas <jotap@MacBook-Pro-de-Jean.local>
2017-10-16 16:24:01 -0300

    se modifica el front, dejando el estilo entregado por carlos dedios

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'ruby-oci8'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'devise'
+gem 'jquery-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,14 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'dist', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'dist', 'css', 'skins')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'dist', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'dist', 'img')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'dist', 'img', 'credit')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'js')
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
