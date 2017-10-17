e54debe8a60bb4faf53033ddedcad4ed20af3df1
Jean Pierre Perez Varas <jp.perezvaras@gmail.com>
2017-10-17 14:46:26 -0300

    configurando archivo deploy para rama test

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,8 @@ set :repo_url, "git@gitlab.mesos.cl:Mesos/comisiones.git"
 
 
 # Default branch is :master
- ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
+ #ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ set :branch, "test"
 # Default deploy_to directory is /var/www/my_app_name
  set :deploy_to, "/var/www/comisiones"
 
